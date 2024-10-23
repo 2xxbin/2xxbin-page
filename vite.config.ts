16c1ff87f5365da1af18faa26857a846f86e0259
2xxbin <2xxbin1208@gmail.com>
2024-10-23 16:33:19 +0900

    Set absolute path

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	resolve: {
+		alias: [
+			{find: "@", replacement: "/src"},
+			{find: "@routes", replacement: "/src/routes"},
+			{find: "@components", replacement: "/src/components"},
+		]
+	}
 });
