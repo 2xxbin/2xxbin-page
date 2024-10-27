9bb64ed3da317974225dfb74d3c38d79ef995068
2xxbin <2xxbin1208@gmail.com>
2024-10-27 12:48:01 +0900

    Add Tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+import typography from '@tailwindcss/typography'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/views/**/*.{vue,js,ts,jsx,tsx,md}',
+    './src/components/**/*.{vue,js,ts,jsx,tsx,md}',
+  ],
+  theme: {
+    extend: {},
+    screens: {
+      sm: { min: '390px', max: '819px' },
+      md: { min: '820px' },
+      lg: { min: '1080px' },
+    },
+    fontFamily: {
+      sans: ['Noto Sans KR', 'Roboto', 'ui-sans-serif', 'system-ui'],
+    },
+  },
+  plugins: [typography],
+}
