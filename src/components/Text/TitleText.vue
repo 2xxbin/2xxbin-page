d5b8a6e942c085954e6a49463be705a31037789e
2xxbin <2xxbin1208@gmail.com>
2024-10-27 13:28:04 +0900

    Add Home Page Layout

---
+++ src/components/Text/TitleText.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const { isCenterText } = defineProps<{
+  isCenterText?: boolean
+}>()
+</script>
+
+<template>
+  <h1
+    :class="[
+      'font-bold',
+      'text-5xl',
+      ' mb-12',
+      isCenterText ? 'text-center' : '',
+    ]"
+  >
+    <slot />
+  </h1>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import CenterContainer from '@/components/Container/CenterContainer.vue'
+import TitleText from '@/components/Text/TitleText.vue'
+</script>
+
+<template>
+  <CenterContainer>
+    <TitleText :is-center-text="true">Home</TitleText>
+  </CenterContainer>
+</template>
