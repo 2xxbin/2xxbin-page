8ec0dc9bed5581a8fb2a51fdd3197f28cdffacc4
2xxbin <2xxbin1208@gmail.com>
2024-10-27 13:53:23 +0900

    Add Util Markdown.ts

---
+++ src/utils/markdown.ts
@@ -0,0 +1,24 @@
+import hljs from 'highlight.js'
+import { marked, type MarkedOptions } from 'marked'
+import { markedHighlight, type SynchronousOptions } from 'marked-highlight'
+
+const markedHighlightConfig: SynchronousOptions = {
+  langPrefix: 'hljs language-',
+  highlight(code, lang) {
+    const language = hljs.getLanguage(lang) ? lang : 'plaintext'
+    return hljs.highlight(code, { language }).value
+  },
+}
+
+const markedConfig: MarkedOptions = {
+  async: true,
+  pedantic: false,
+  gfm: true,
+}
+
+export const markdownToHtml = (markdownContent: string) => {
+  marked.use(markedHighlight(markedHighlightConfig))
+
+  const output = marked(markdownContent, markedConfig)
+  return output
+}
