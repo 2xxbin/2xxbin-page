f2ebefcb99ccda16563af295b7f65a83ac3fd331
2xxbin <2xxbin1208@gmail.com>
2024-10-27 12:53:51 +0900

    Add Header

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header
+    class="h-20 flex sm:flex-col justify-between sm:justify-center items-center px-8 sm:pt-8 sm:mb-12"
+  >
+    <a href="/" class="text-xl sm:text-2xl text-center font-bold sm:mb-2"
+      ><span>2xxbin</span></a
+    >
+    <nav
+      class="w-2/5 sm:w-full flex items-center justify-end sm:justify-center [&_a]:mx-4 [&_a]:text-center"
+    >
+      <a href="/">Home</a> | <a href="/notices">Notices</a> |
+      <a href="/voicebanks">Voice Banks</a> |
+      <a href="/reclists">Record Lists</a>
+    </nav>
+  </header>
+</template>
